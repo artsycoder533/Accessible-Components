bef41fb5e694eadf36d8e764b6882b7166785062
Natasha <njohnson533@gmail.com>
2022-02-01 10:48:15 -0500

    added show/hide password button to login form

--- Form Controls/login form/script.js
+++ Form Controls/login form/script.js
@@ -1,6 +1,8 @@
 const honeypotInput = document.getElementById("honeypot");
 const form = document.querySelector(".login-form");
 const loginBtn = document.getElementById("login");
+const showBtn = document.getElementById("show");
+const passwordInput = document.getElementById("password");
 
 //check if filled in, reset form
 const checkForHoneypot = () => {
@@ -14,4 +16,16 @@ const validateForm = (e) => {
     checkForHoneypot();
 }
 
+const toggleShowBtn = () => {
+    if (showBtn.innerText === "SHOW") {
+        showBtn.innerText = "HIDE";
+        passwordInput.type = "text";
+    }
+    else {
+        showBtn.innerText = "SHOW";
+        passwordInput.type = "password";
+    }
+}
+
 loginBtn.addEventListener("click", validateForm);
+showBtn.addEventListener("click", toggleShowBtn);
