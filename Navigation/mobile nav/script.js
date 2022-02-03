008a5cc3cbe867d67c0da31bcfd069401b12e37a
Natasha <njohnson533@gmail.com>
2022-02-02 22:29:42 -0500

    added hamburger button animations

--- Navigation/mobile nav/script.js
+++ Navigation/mobile nav/script.js
@@ -1,13 +1,14 @@
 const hamburgerBtn = document.getElementById("hamburger");
+const hamburgerLine = document.querySelector(".hamburger-line");
 
 const toggleNavMenu = () => {
-    if (hamburgerBtn.getAttribute("aria-expanded") === false) {
-        hamburgerBtn.setAttribute("aria-expanded", true);
+    if (hamburgerBtn.getAttribute("aria-expanded") === "false") {
+        hamburgerBtn.setAttribute("aria-expanded", "true");
     }
     else {
-        hamburgerBtn.setAttribute("aria-expanded", false);
+        hamburgerBtn.setAttribute("aria-expanded", "false");
     }
-    
+    hamburgerLine.classList.toggle("rotate");
 }
 
 hamburgerBtn.addEventListener("click", toggleNavMenu);
